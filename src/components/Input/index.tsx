c030c7f9f5402e3900ab74156142abe4a25af8d4
Patrick Mazzuco <patrick.flores@acad.pucrs.br>
2021-07-14 14:36:14 -0300

    refactor: update ModalAddFood and Input to function component w/ typecript

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,16 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
-
+import { useEffect, useRef, useState, useCallback } from 'react';
 import { useField } from '@unform/core';
+import { IconType } from 'react-icons';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: IconType;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 export interface Food {
   id: number;
   name: string;
+  description: string;
   price: string;
   available: boolean;
   image: string;
